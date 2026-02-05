9a485450db3a04f4c75063821e9e6078f011f11c
prasannapp-uiux <prasannapp.in@gmail.com>
2026-02-05 21:23:48 +0530

    header title updated

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <h1 className="text-4xl font-bold text-blue-600 bg-red-500 p-5">
-      Tailwind is Working 🚀
+      Employee management dashboard
     </h1>
   );
 };
