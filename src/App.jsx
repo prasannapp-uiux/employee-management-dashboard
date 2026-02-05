b1f3b0a6ca5bb6de040ea437f63cc01a5155b07d
prasannapp-uiux <prasannapp.in@gmail.com>
2026-02-05 21:06:03 +0530

    tailwind css installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Employee Management Dashboard</h1>;
-}
+import Header from "./components/layout/header";
 
+function App() {
+  return (
+    <>
+    <h1 className="text-4xl font-bold text-blue-600">Employee Management Dashboard</h1>
+    <Header/>
+    </>
+  )
+}
+<Header/>
 export default App;
---
+++ src/components/layout/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600 bg-red-500 p-5">
+      Tailwind is Working 🚀
+    </h1>
+  );
+};
+
+export default Header;
